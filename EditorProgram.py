b891ace8e0cbd0a7ccb09e77ac18cbc38b9ec08d
aadiltajani <61664466+aadiltajani@users.noreply.github.com>
2020-05-08 17:32:54 +0530

    Update EditorProgram.py

--- EditorProgram.py
+++ EditorProgram.py
@@ -2,36 +2,37 @@ import fnmatch
 import os
 import pytesseract
 import zipfile
-import platform 
+import platform
 
-z = zipfile.ZipFile("input.docx")
-
-#print all files in zip archive
-all_files = z.namelist()
-
-#get all files in word/media/ directory
+input_type = input("What is the source ?(doc/image): ")
 
 
 if platform.system() == 'Windows':
-
-    images = filter(lambda x: x.startswith('word\\media\\'), all_files)
-
+    sep = '\\'
 else:
-    
+    sep = '/'
+
+
+if input_type == 'doc':
+    z = zipfile.ZipFile("input.docx")
+    all_files = z.namelist()
+
+    # get all files in word/media/ directory
     images = filter(lambda x: x.startswith('word/media/'), all_files)
 
+    # print all files in zip archive
+    for image in images:
+        print(image)
+        curr_image = z.open(image).read()
+        f = open("image1.jpeg", 'wb')
+        f.write(curr_image)
+        z.extract(image, 'Images')
+    listOfFiles = os.listdir('Images{0}word{0}media'.format('\\'))
 
-for image in images :
-    print(image)
-    curr_image = z.open(image).read()
-    f = open("image1.jpeg",'wb')
-    f.write(curr_image)
-    z.extract(image, r'Images')
+else:
+    listOfFiles = os.listdir('Images')
 
 
-
-listOfFiles = os.listdir(r'Images/word/media')
-print(listOfFiles)
 pattern_list = ["*.png", "*.jpg", "*.jpeg"]
 with open('file_names.txt', 'w') as names:
 
@@ -41,19 +42,21 @@ with open('file_names.txt', 'w') as names:
 
             if fnmatch.fnmatch(entry, pattern):
 
+                if input_type == 'doc':
 
-                if platform.system() == 'Windows':
-
-                    names.write("Images\\word\\media\\" + entry+"\n")
+                    names.write("Images{0}word{0}media{0}".format(sep) + entry + "\n")
 
                 else:
+                    names.write("Images{0}".format(sep) + entry + "\n")
 
-                    names.write("Images/word/media/" + entry+"\n")
-               
-                names.writelines("Images\\" + entry+"\n")
 
-# SET PATH BELOW FOR WINDOWS ONLY
-# pytesseract.pytesseract.tesseract_cmd = r'<path to Tesseract-OCR\tesseract.exe>'
+
+######### SET PATH BELOW FOR WINDOWS ONLY
+
+# pytesseract.pytesseract.tesseract_cmd = '<PATH to \\Tesseract-OCR\\tesseract.exe   generally in C:\\ProgramFiles(
+# x86) or where you install binary file>' 
+
+
 text = pytesseract.image_to_string('file_names.txt')
 
 while (True):
@@ -67,4 +70,5 @@ while (True):
 
 with open('Changed_files.doc', 'w') as f:
     f.write(text)
+
 print("File \"Changed_files.doc\" created !!!")
